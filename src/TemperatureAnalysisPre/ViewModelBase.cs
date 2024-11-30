7a5ab44ebd86f7bbc4482289aea74c9012f595d3
liumenghui <liumenghui@sofarsolar.com>
2024-11-30 16:02:15 +0800

    加入MVVM框架

---
+++ src/TemperatureAnalysisPre/ViewModelBase.cs
@@ -0,0 +1,83 @@
+﻿namespace TemperatureAnalysisPre;
+
+public abstract class ViewModelBase : BindableBase, 
+    INavigationAware, 
+    IRegionMemberLifetime, 
+    IConfirmNavigationRequest
+{
+    /// <summary>
+    /// 导航器
+    /// </summary>
+    public IRegionManager? Region;
+
+    /// <summary>
+    /// 弹窗服务
+    /// </summary>
+    public IDialogService? Dialog;
+
+    /// <summary>
+    /// 事件聚合器
+    /// </summary>
+    public IEventAggregator? Event;
+
+    private DelegateCommand? _LoadedCommand;
+
+    public DelegateCommand LoadedCommand =>
+        _LoadedCommand ?? (_LoadedCommand = new DelegateCommand(ExecuteLoadedCommand));
+
+    public ViewModelBase()
+    {
+    }
+
+    public ViewModelBase(IContainerExtension container)
+    {
+        Region = container.Resolve<IRegionManager>();
+        Dialog = container.Resolve<IDialogService>();
+        Event = container.Resolve<IEventAggregator>();
+    }
+
+
+
+    /// <summary>
+    ///初始化界面加载
+    /// </summary>
+    public virtual void ExecuteLoadedCommand()
+    {
+    }
+
+    /// <summary>
+    /// 标记上一个视图时候被销毁
+    /// </summary>
+    public bool KeepAlive => false;
+
+    public virtual void ConfirmNavigationRequest(NavigationContext navigationContext, Action<bool> continuationCallback)
+    {
+        continuationCallback(true);
+    }
+
+    /// <summary>
+    /// 导航后的目标视图是否缓存
+    /// </summary>
+    /// <param name="navigationContext"></param>
+    /// <returns></returns>
+    public virtual bool IsNavigationTarget(NavigationContext navigationContext)
+    {
+        return false;
+    }
+
+    /// <summary>
+    /// 导航前
+    /// </summary>
+    /// <param name="navigationContext"></param>
+    public virtual void OnNavigatedFrom(NavigationContext navigationContext)
+    {
+    }
+
+    /// <summary>
+    /// 导航后
+    /// </summary>
+    /// <param name="navigationContext"></param>
+    public virtual void OnNavigatedTo(NavigationContext navigationContext)
+    {
+    }
+}
---
+++ src/TemperatureAnalysisPre/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace TemperatureAnalysisPre.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _Message = "";
+
+        public string Message
+        {
+            get { return _Message; }
+            set { SetProperty(ref _Message, value); }
+        }
+
+        public MainWindowViewModel(IContainerExtension container) : base(container)
+        {
+        }
+
+        #region 视图属性
+
+        private bool _Network = true;
+
+        public bool Network
+        {
+            get { return _Network; }
+            set { SetProperty(ref _Network, value); }
+        }
+
+        private string _title = "欢迎";
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private WindowState _WindowState;
+
+        /// <summary>
+        /// 窗体状态
+        /// </summary>
+        public WindowState WindowState
+        {
+            get { return _WindowState; }
+            set { _WindowState = value; RaisePropertyChanged(); }
+        }
+
+        private Thickness _GlassFrameThickness;
+
+        public Thickness GlassFrameThickness
+        {
+            get { return _GlassFrameThickness; }
+            set { _GlassFrameThickness = value; RaisePropertyChanged(); }
+        }
+
+        #endregion 视图属性
+
+        #region 窗体命令
+
+        private DelegateCommand<string> _GoBrowser;
+
+        public DelegateCommand<string> GoBrowser =>
+            _GoBrowser ?? (_GoBrowser = new DelegateCommand<string>(ExecuteGoBrowser));
+
+        private void ExecuteGoBrowser(string uri)
+        {
+            Process.Start(new ProcessStartInfo(uri));
+        }
+
+        #endregion 窗体命令
+
+        #region 核心方法
+
+        public override void ExecuteLoadedCommand()
+        {
+            base.ExecuteLoadedCommand();
+        }
+
+        #endregion 核心方法
+    }
+}
